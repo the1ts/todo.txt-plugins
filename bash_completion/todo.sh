40718686777b8d2220cce9c50a25817a11bc1efa
Paul Mansfield <paulbm@dellboy.mansfield.net>
2009-09-08 22:03:00 +0100

    Added complete for projects and contexts

--- bash_completion/todo.sh
+++ bash_completion/todo.sh
@@ -1,24 +1,30 @@
-# -*- mode: shell-script; sh-basic-offset: 8; indent-tabs-mode: t -*-
-# ex: ts=8 sw=8 noet filetype=sh
-#
 # todo.sh(1) completion
 # Copyright 2003 Eelco Lempsink <eelcolempsink@gmx.net>
 # License: GNU GPL v2 or later
 
-_todo.sh()
+function _todo.sh()
 {
-	local cur
-
+	#Find how we are being run
+	our_cmd=${COMP_WORDS[0]}
+	cmd=${COMP_CWORD} 
 	COMPREPLY=()
-	#cur=`_get_cword`
-	cur="${COMP_WORDS[COMP_CWORD]}"
-
-	case "$cur" in
-	    *)
-		    COMPREPLY=( $( compgen -W "`todo.sh  -h| grep "^    "| awk '{print $1}'| sed 's/|/ /g' ; ls $HOME/.todo.actions.d/` " -- $cur ) )
+	word="${COMP_WORDS[COMP_CWORD]}"
+	if ((cmd==1)); then
+		# Help me pick a todo.sh action
+		COMPREPLY=($( compgen -W "`todo.sh  -h| grep "^    "| awk '{print $1}'| sed 's/|/ /g' ; ls $HOME/.todo.actions.d/`" -- "$word"));
+	else
+		# help me pick a project or context
+		case ${COMP_WORDS[1]} in
+			"lsprj"|"listproj")
+		COMPREPLY=($(compgen -W "$(eval $our_cmd lsprj | tr -d '\r')" -- "${word}"));
 		;;
-	esac
-
-	return 0
+			"lsc"|"listcon")
+		COMPREPLY=($(compgen -W "$(eval $our_cmd lsc | tr -d '\r')" -- "${word}"));
+		;;
+			"add"|"addto"|"append"|"prepend"|"replace")
+		COMPREPLY=($(compgen -W "$(eval $our_cmd lsprj | tr -d '\r') $(eval $our_cmd lsc | tr -d '\r')" -- "${word}"));
+		;;
+		esac
+	fi
 }
-complete -F _todo.sh $default todo.sh
+complete -F _todo.sh todo.sh t tt tw tp
