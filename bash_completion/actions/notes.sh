a6defa4fbbd7c3c60aed54581669e538802a3fbf
Paul Mansfield <paul.mansfield@mansteck.co.uk>
2013-04-12 14:17:46 +0100

    Change to newer bash tests

--- bash_completion/actions/notes.sh
+++ bash_completion/actions/notes.sh
@@ -1,14 +1,14 @@
 # notesedit 
-if [ ${COMP_WORDS[1]} = "notesedit" -o ${COMP_WORDS[1]} = "ne" ]; then
+if [[ ${COMP_WORDS[1]} = "notesedit" ]] || [[ ${COMP_WORDS[1]} = "ne" ]]; then
     COMPREPLY=($(compgen -W "$(eval $our_cmd listnotes)" -- "${word}"));
 fi
 
 # notescat
-if [ ${COMP_WORDS[1]} = "notescat" -o ${COMP_WORDS[1]} = "nc" ]; then
+if [[ ${COMP_WORDS[1]} = "notescat" ]] || [[ ${COMP_WORDS[1]} = "nc" ]]; then
     COMPREPLY=($(compgen -W "$(eval $our_cmd listnotes)" -- "${word}"));
 fi
 
 # notesarchive
-if [ ${COMP_WORDS[1]} = "notearchive" -o ${COMP_WORDS[1]} = "na" ]; then
+if [[ ${COMP_WORDS[1]} = "notearchive" ]] || [[ ${COMP_WORDS[1]} = "na" ]]; then
     COMPREPLY=($(compgen -W "$(eval $our_cmd listnotes)" -- "${word}"));
 fi
