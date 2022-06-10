91229fbf04f17e8f899cdafffcd49907406b68e5
Paul Mansfield <paul.mansfield@mansteck.co.uk>
2022-06-10 13:25:40 +0100

    Enhancement: Create single encrypted note action
    - Single enote action that does job of all previous enote type actions as options
    - -Bash completion for single enotes action
    - Tests for single action and bash completion

--- bash_completion/actions/enotes.sh
+++ bash_completion/actions/enotes.sh
@@ -1,17 +1,30 @@
-# notesedit
-if [[ ${COMP_WORDS[1]} =~ (enotesedit|ene) ]] ; then
-	# shellcheck disable=SC2207,SC2086
-	COMPREPLY=($(compgen -W "$(eval $our_cmd listenotes)" -- "${word}"));
-fi
-
-# notescat
-if [[ ${COMP_WORDS[1]} =~ (enotescat|enc) ]]; then
-	# shellcheck disable=SC2207,SC2086
-	COMPREPLY=($(compgen -W "$(eval $our_cmd listenotes)" -- "${word}"));
-fi
-
-# notesarchive
-if [[ ${COMP_WORDS[1]} =~ (enotesarchive|ena) ]]; then
-	# shellcheck disable=SC2207,SC2086
-	COMPREPLY=($(compgen -W "$(eval $our_cmd listenotes)" -- "${word}"));
+# shellcheck shell=bash
+COMMANDS="add archive cat edit list listarchived rename unarchive"
+cmd=${COMP_CWORD}
+our_cmd=${COMP_WORDS[0]}
+# Only capture todo.sh notes
+if [[ ${COMP_WORDS[1]} = enotes ]]; then
+  # Give commands as first complete
+  if ((cmd==2)); then
+    # shellcheck disable=SC2207,SC2086
+	  COMPREPLY=($(compgen -W "$(eval echo $COMMANDS)" -- "${word}"));
+  else
+    # If add give back list of items 1 to END
+    if [[ ${COMP_WORDS[2]} = add ]]; then
+      # shellcheck disable=SC2086
+      END=$(TODOTXT_VERBOSE=0 eval ${our_cmd} list | wc -l | cut -d ' ' -f 1)
+      # shellcheck disable=SC2207,SC2086,SC2175
+      COMPREPLY=($(compgen -W "$(eval echo {1..$END} | tr '\n' ' ')" -- "${word}"));
+      # If add and number give list of notes
+      if [[ ${COMP_WORDS[3]} =~ .*[0-9] ]]; then
+        # shellcheck disable=SC2207,SC2086
+        COMPREPLY=($(compgen -W "$(eval ${our_cmd} lsen | tr -d '\r')" -- "${word}"));
+      fi
+    fi
+    # If cat, edit, rename or unarchive give list of notes
+    if [[ ${COMP_WORDS[2]} =~ (cat|edit|rename|unarchive) ]]; then
+    # shellcheck disable=SC2207,SC2086
+      COMPREPLY=($(compgen -W "$(eval ${our_cmd} lsen | tr -d '\r')" -- "${word}"));
+    fi
+  fi
 fi
