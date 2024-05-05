6ee839bad3adce10bdc3ca5cf0999d018efab508
Paul Mansfield <paul.mansfield@mansteck.co.uk>
2024-05-05 17:27:12 +0100

    bash_completion fixes
    - Shellcheck fixes
    - fix project discovery (lsprj)

--- bash_completion/actions/projectrename.sh
+++ bash_completion/actions/projectrename.sh
@@ -1,23 +1,21 @@
 # Projectrename first options (List of all projects)
-if [[ ${COMP_WORDS[1]} =~ (projectrename|prr) ]] && \
+our_cmd=${COMP_WORDS[0]}
+word="${COMP_WORDS[COMP_CWORD]}"
+if [[ ${COMP_WORDS[1]} =~ (projectrename|prr) ]] &&
 	[[ ${#COMP_WORDS[@]} -eq 3 ]]; then
 	# shellcheck disable=SC2207,SC2086,SC2030
-	COMPREPLY=($(compgen -W "$(export TODOTXT_PLAIN=1; eval $our_cmd listproj \
-		| sort -n)" -- "${word}"));
+	mapfile -t COMPREPLY < <(compgen -W "$(eval "${our_cmd}" lsprj)" -- "${word}")
 fi
 # Projectrename second options (List of all projects the one we are renaming)
-if [[ ${COMP_WORDS[1]} =~ (projectrename|prr) ]] && \
+if [[ ${COMP_WORDS[1]} =~ (projectrename|prr) ]] &&
 	[[ ${#COMP_WORDS[@]} -eq 4 ]]; then
 	# shellcheck disable=SC2207,SC2086,SC2030,SC2031
-	COMPREPLY=($(compgen -W "$(export TODOTXT_PLAIN=1; eval $our_cmd listproj \
-		| sort -n | grep -v ${COMP_WORDS[2]})" -- "${word}"));
+	mapfile -t COMPREPLY < <(compgen -W "$(eval "${our_cmd}" lsprj | grep -v ${COMP_WORDS[2]})" -- "${word}")
 fi
-# Projectrename third options 
+# Projectrename third options
 # (List all items containing the project we are renaming)
-	# shellcheck disable=SC2207,SC2086,SC2030,SC2031
-if [[ ${COMP_WORDS[1]} =~ (projectrename|prr) ]] && \
+# shellcheck disable=SC2207,SC2086,SC2030,SC2031
+if [[ ${COMP_WORDS[1]} =~ (projectrename|prr) ]] &&
 	[[ ${#COMP_WORDS[@]} -eq 5 ]]; then
-	COMPREPLY=($(compgen -W "$(export TODOTXT_PLAIN=1; \
-		eval $our_cmd ls ${COMP_WORDS[2]} | awk '/^[0-9]/{print $1}' \
-		| tr -d '\r' | sort -n)" -- "${word}"));
+	mapfile -t COMPREPLY < <(compgen -W "$(eval "${our_cmd}" ls ${COMP_WORDS[2]} | awk '/^[0-9]/{print $1}')" -- "${word}")
 fi
