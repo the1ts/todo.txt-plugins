5243470e737d998ca3da6275b6e7e3a2f4042fc1
Paul Mansfield <paul.mansfield@mansteck.co.uk>
2026-05-28 13:03:48 +0100

    Better coverage for projectview tests
    - more coverage of mixtures and multiples

--- tests/t5020-projectview.sh
+++ tests/t5020-projectview.sh
@@ -7,9 +7,9 @@ test_description='projectview action functionality
 export TODO_ACTIONS_DIR=$TEST_DIRECTORY/../actions/projectview
 
 cat >todo.txt <<EOF
-Buy tools +purchase
-Fix bicycle +repair
-Ride bike
+Buy tools +purchase @pc
+Fix bicycle +repair @garage
+Ride bike @grass
 EOF
 
 test_todo_session 'projectview show usage' <<EOF
@@ -39,35 +39,73 @@ test_todo_session 'projectview no term' <<EOF
 >>> todo.sh projectview | sed '/^$/d'
 =====  Projects  =====
 ---  purchase  ---
-1 Buy tools +purchase
+1 Buy tools +purchase @pc
 ---  repair  ---
-2 Fix bicycle +repair
+2 Fix bicycle +repair @garage
 --- Not in projects ---
-3 Ride bike
+3 Ride bike @grass
 === 0
 EOF
 
 test_todo_session 'projectview with context as term' <<EOF
->>> todo.sh projectview +purchase | sed '/^$/d'
+>>> todo.sh projectview @pc | sed '/^$/d'
 =====  Projects  =====
 ---  purchase  ---
-1 Buy tools +purchase
+1 Buy tools +purchase @pc
 === 0
 EOF
 
-test_todo_session 'projectview with not a context term' <<EOF
+test_todo_session 'projectview with word as term' <<EOF
 >>> todo.sh projectview bike | sed '/^$/d'
 =====  Projects  =====
 --- Not in projects ---
-3 Ride bike
+3 Ride bike @grass
 === 0
 EOF
 
-test_todo_session 'projectview with multiple terms' <<EOF
->>> todo.sh projectview tools +purchase | sed '/^$/d'
+test_todo_session 'projectview with project as term' <<EOF
+>>> todo.sh projectview +purchase | sed '/^$/d'
 =====  Projects  =====
 ---  purchase  ---
-1 Buy tools +purchase
+1 Buy tools +purchase @pc
+=== 0
+EOF
+
+test_todo_session 'projectview with word and project, word overrides project as its more specific' <<EOF
+>>> todo.sh projectview +purchase bicycle | sed '/^$/d'
+=====  Projects  =====
+---  repair  ---
+2 Fix bicycle +repair @garage
+=== 0
+EOF
+
+test_todo_session 'projectview with muliple project (or not and)' <<EOF
+>>> todo.sh projectview +purchase +repair | sed '/^$/d'
+=====  Projects  =====
+---  purchase  ---
+1 Buy tools +purchase @pc
+---  repair  ---
+2 Fix bicycle +repair @garage
+=== 0
+EOF
+
+test_todo_session 'projectview with muliple search terms (and, not or), in a single item, note case insensitive' <<EOF
+>>> todo.sh projectview fix bicycle | sed '/^$/d'
+=====  Projects  =====
+---  repair  ---
+2 Fix bicycle +repair @garage
+=== 0
+EOF
+
+test_todo_session 'projectview with muliple search terms (and, not or), not in single item, but if terms are or not and would find two lines' <<EOF
+>>> todo.sh projectview tools buy | sed '/^$/d'
+      "tools buy" not found in todo.txt
+    projectview [TERM...]
+      Show todo items containing TERM, grouped by project, and displayed
+      in priority order. If no TERM provided, displays entire todo.txt
+      in project sections.
+      If any TERMs are not a project it will override all that are a project
+      as they are more specific than projects
 === 0
 EOF
 
@@ -93,14 +131,6 @@ test_todo_session 'projectview with project not in todo.txt' <<EOF
       If any TERMs are not a project it will override all that are a project
       as they are more specific than projects
 === 1
-EOF
-
-test_todo_session 'projectview with term and project, term overrides project as its more specific' <<EOF
->>> todo.sh projectview +purchase bicycle | sed '/^$/d'
-=====  Projects  =====
----  repair  ---
-2 Fix bicycle +repair
-=== 0
 EOF
 
 test_done
