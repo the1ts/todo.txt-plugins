36c162a8606019271205066b427816a587a9a79a
Paul Mansfield <paul.mansfield@mansteck.co.uk>
2026-06-12 14:05:22 +0100

    Better bash completion for edit
    - edit done now has line completion for done file

--- bash_completion/actions/edit.sh
+++ bash_completion/actions/edit.sh
@@ -1,11 +1,14 @@
 # shellcheck shell=bash
 # edit
 if [[ ${COMP_WORDS[1]} = "edit" ]]; then
-	# shellcheck disable=SC2207,SC2086,SC2154
-	mapfile -t COMPREPLY < <(compgen -W "$(eval echo "todo done cfg")" -- "${word}")
-	if [[ ${COMP_WORDS[2]} ]]; then
-		# shellcheck disable=SC2207,SC2175,SC2086
-		mapfile -t COMPREPLY < <(compgen -o nosort -W "$(eval echo "+{1..$END}")" -- "${word}")
-	fi
+  # shellcheck disable=SC2207,SC2086,SC2154
+  mapfile -t COMPREPLY < <(compgen -W "$(eval echo "todo done cfg")" -- "${word}")
+  if [[ ${COMP_WORDS[2]} = "todo" ]]; then
+    # shellcheck disable=SC2207,SC2175,SC2086
+    mapfile -t COMPREPLY < <(compgen -o nosort -W "$(eval echo "{1..$END}")" -- "${word}")
+  fi
+  if [[ ${COMP_WORDS[2]} = "done" ]]; then
+    # shellcheck disable=SC2207,SC2175,SC2086
+    mapfile -t COMPREPLY < <(compgen -o nosort -W "$(eval echo "{1..$DONE_END}")" -- "${word}")
+  fi
 fi
-
