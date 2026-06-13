6ac7657016ebec26c64c2fbf5f3bc29c80f01b25
Paul Mansfield <paul.mansfield@mansteck.co.uk>
2026-06-13 17:38:22 +0100

    better logic for edit file todo and done completion

--- bash_completion/actions/edit.sh
+++ bash_completion/actions/edit.sh
@@ -1,14 +1,13 @@
 # shellcheck shell=bash
 # edit
+# We are in the edit action, return allowed files
 if [[ ${COMP_WORDS[1]} = "edit" ]]; then
   # shellcheck disable=SC2207,SC2086,SC2154
   mapfile -t COMPREPLY < <(compgen -W "$(eval echo "todo done cfg")" -- "${word}")
+  # If todo or done allow line number completion for each
   if [[ ${COMP_WORDS[2]} = "todo" ]]; then
-    # shellcheck disable=SC2207,SC2175,SC2086
     mapfile -t COMPREPLY < <(compgen -o nosort -W "$(eval echo "{1..$END}")" -- "${word}")
-  fi
-  if [[ ${COMP_WORDS[2]} = "done" ]]; then
-    # shellcheck disable=SC2207,SC2175,SC2086
+  elif [[ ${COMP_WORDS[2]} = "done" ]]; then
     mapfile -t COMPREPLY < <(compgen -o nosort -W "$(eval echo "{1..$DONE_END}")" -- "${word}")
   fi
 fi
